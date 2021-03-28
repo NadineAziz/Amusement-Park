e90da843af06434573e4c2b13b013a787202d94e
NadeenHussein <nadeenahmed_a@yahoo.com>
2021-03-28 11:35:49 +0300

    feat: select game once from panel

--- src/amusement/park/GameArea.java
+++ src/amusement/park/GameArea.java
@@ -49,6 +49,7 @@ public class GameArea extends JPanel{
     }
 
     private boolean addBuilding(BasicBuilding building, int indexX, int indexY) {
+       if (!checkIfBuildingExists(building) ) {
         if (indexX < numberOfCols && indexY < numberOfRows) {
             if (placesMatrix[indexX][indexY] == null) {
                 placesMatrix[indexX][indexY] = building;
@@ -59,6 +60,24 @@ public class GameArea extends JPanel{
         } else {
             return false;
         }
+        }else {
+                return false;
+        }
+    }
+
+        private boolean checkIfBuildingExists(BasicBuilding building) {
+        System.out.println( building.getClass().getName());
+        for (int i= 0; i < numberOfCols; i ++) {
+            for (int j = 0; j < numberOfRows; j++) {
+                building.getClass().getName();
+                
+              if (placesMatrix[i][j] != null && building.getClass().getName().equals(placesMatrix[i][j].getClass().getName())) {
+                  return true;
+              }
+              
+            }
+        }
+        return false;
     }
 
 
