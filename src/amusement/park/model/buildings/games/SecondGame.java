0ef26dbb0c29d9c8dbfdb982403f3a62ddc0e502
Jamala Khaligova <jamalakhaligova@gmail.com>
2021-03-29 10:41:45 +0200

    fix:Multiple buildings added

--- src/amusement/park/model/buildings/games/SecondGame.java
+++ src/amusement/park/model/buildings/games/SecondGame.java
@@ -3,14 +3,10 @@ package amusement.park.model.buildings.games;
 import amusement.park.model.buildings.Building;
 
 
-public class SecondGame extends Building {
+public class SecondGame extends BaseGame {
     
     public SecondGame() {
         super("games/game2.png", 5, 10, 10);
     }
 
-        @Override
-    public Object clone() {
-        return new FirstGame();
-    }
 }
