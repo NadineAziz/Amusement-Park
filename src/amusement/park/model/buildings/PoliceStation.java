10d3584d1f21355ce38aec0a571e2e00310e4eda
Jamala Khaligova <jamalakhaligova@gmail.com>
2021-04-25 19:12:01 +0200

    fix:merge issues

--- src/amusement/park/model/buildings/PoliceStation.java
+++ src/amusement/park/model/buildings/PoliceStation.java
@@ -3,7 +3,7 @@ package amusement.park.model.buildings;
 
 public class PoliceStation extends BasicBuilding {
     public PoliceStation() {
-        super("police_station.jpg", -1, 2,"PoliceStation");
+        super("entrance.jpg", -1, 2,"PoliceStation");
     }
 
 }
