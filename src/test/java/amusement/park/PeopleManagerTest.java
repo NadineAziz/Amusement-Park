e604dff52e7083ece2abf1d31bd8c0a9b29aea20
Matin <matinaliyev250501@gmail.com>
2021-05-10 11:51:45 +0200

    fix

--- src/test/java/amusement/park/PeopleManagerTest.java
+++ src/test/java/amusement/park/PeopleManagerTest.java
@@ -17,7 +17,7 @@ import static org.junit.Assert.*;
 public class PeopleManagerTest {
     @Test
     public void test_getNumOfThieves() {
-        PeopleManager peopleManager = (PeopleManager) new PeopleManagerImpl();
+        PeopleManagerImpl peopleManager =  new PeopleManagerImpl();
         
         assertTrue(peopleManager.getNumberOfThieves()==1);
         assertEquals(1, (int) peopleManager.getNumberOfThieves());
@@ -25,21 +25,21 @@ public class PeopleManagerTest {
 
     @Test
    public void test_getNumofsecurities() {
-        PeopleManager peopleManager = (PeopleManager) new PeopleManagerImpl();
+        PeopleManagerImpl peopleManager =  new PeopleManagerImpl();
         
         assertTrue(peopleManager.getNumofsecurities()==2);
-        assertEquals(2, (int) peopleManager.getNumberOfThieves());
+        assertEquals(2, (int) peopleManager.getNumofsecurities());
     }
      public void test_getNumberOfQuests() {
         PeopleManager peopleManager = (PeopleManager) new PeopleManagerImpl();
         
         assertTrue(peopleManager.getNumberOfQuests()==3);
-        assertEquals(3, (int) peopleManager.getNumberOfThieves());
+        assertEquals(3, (int) peopleManager.getNumberOfQuests());
     }
        public void test_getNumofcops() {
         PeopleManager peopleManager = (PeopleManager) new PeopleManagerImpl();
         
         assertTrue(peopleManager.getNumofcops()==4);
-        assertEquals(4, (int) peopleManager.getNumberOfThieves());
+        assertEquals(4, (int) peopleManager.getNumofcops());
     }
 }
